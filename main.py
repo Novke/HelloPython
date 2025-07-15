f2205a04a8d6487269670eaf51e27fe08b987328
Novke <novke.brate@gmail.com>
2025-07-15 14:02:35 +0200

    bugfix

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 
 router = APIRouter()
+
+@router.get("/hello")
+async def root():
+    return {"message": "Zdravo svet! -Piton"}
+
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://workana-premium.workable.com"],  # 👈 or ["*"] for any origin (not recommended for production)
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
-@router.get("/")
-async def root():
-    return {"message": "Zdravo svet! -Piton"}
+app.include_router(router)
